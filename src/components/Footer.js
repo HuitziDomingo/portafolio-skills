44df838d10cc8ed71c9d928d8471979f50ae61fd
Huitzitzili Domingo <huitzitzili@spacetime.mx>
2020-10-13 19:32:29 -0500

    cambios pruena 1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,10 +12,10 @@ export default () => {
                     <div className="col l4 offset-l2 s12">
                         <h5 className="white-text">Links</h5>
                         <ul>
-                            <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                            <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                            <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                            <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                            <li><a className="grey-text text-lighten-3" href="#!">Facebook</a></li>
+                            <li><a className="grey-text text-lighten-3" href="#!">Instagram</a></li>
+                            <li><a className="grey-text text-lighten-3" href="#!">Whats App</a></li>
+                            <li><a className="grey-text text-lighten-3" href="#!">Git Hub</a></li>
                         </ul>
                     </div>
                 </div>
